18712483b08e1ef262d95f096c9b927f0881cfae
Thalisson da Rosa <darosathalisson@gmail.com>
2026-05-02 17:57:05 -0400

    Initial project setup
    - Project created, all default fields

---
+++ RickMortyCharGuide/RickMortyCharGuideTests/RickMortyCharGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyCharGuideTests.swift
+//  RickMortyCharGuideTests
+//
+//  Created by Thalisson da Rosa on 02/05/26.
+//
+
+import Testing
+@testable import RickMortyCharGuide
+
+struct RickMortyCharGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
